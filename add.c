5d659a4466a4fabd9171be7258669f3c23e6b5f9
navin <imnavinkr@gmail.com>
2019-05-18 18:33:45 +0530

    first day of C programming

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter Two digit to add \t");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("The addition of two number is " "%d",c);
+	printf("\n");
+}
---
+++ scan.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("Enter the digit to print ");
+	scanf("%d",&a);
+	printf("The input numuber is" "\t" "%d",a);
+	printf("\n");
+}
