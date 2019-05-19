8b23be238ed02c07178b8e8a7010eb53c18dee46
navin <imnavinkr@gmail.com>
2019-05-19 16:43:07 +0530

    programming in c

---
+++ address.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	printf("------------------------------------------------------------------------\n");
+	printf("Name\t\t: Navin Kumar\n");
+	printf("Address\t\t: Sare Homes,Sector 92,Gurgaon\n");
+	printf("Emaild\t\t: imnavinkr@gmail.com\n");
+	printf("------------------------------------------------------------------------\n");
+}
